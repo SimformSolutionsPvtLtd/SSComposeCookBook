3465f6f9ef67edd929906e1a18e651ab2168b120
Parashar Dhamecha <parashar@simformsolutions.com>
2023-09-29 15:03:06 +0530

    Added examples for all variants

--- app/src/main/java/com/jetpack/compose/learning/readmoretextview/ReadMoreTextViewActivity.kt
+++ app/src/main/java/com/jetpack/compose/learning/readmoretextview/ReadMoreTextViewActivity.kt
@@ -5,6 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
@@ -12,11 +17,20 @@ import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.outlined.AccessAlarm
+import androidx.compose.material.icons.outlined.KeyboardArrowDown
+import androidx.compose.material.icons.outlined.KeyboardArrowUp
+import androidx.compose.material.icons.outlined.Scanner
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.jetpack.compose.learning.R
 import com.jetpack.compose.learning.readmoretextview.components.ExpandableTextView
+import com.jetpack.compose.learning.readmoretextview.data.CollapseConfig
+import com.jetpack.compose.learning.readmoretextview.data.ExpandConfig
 import com.jetpack.compose.learning.theme.AppThemeState
 import com.jetpack.compose.learning.theme.BaseView
 import com.jetpack.compose.learning.theme.SystemUiController
@@ -29,7 +43,11 @@ class ReadMoreTextViewActivity : ComponentActivity() {
             val systemUiController = remember { SystemUiController(window) }
             val appTheme = remember { mutableStateOf(AppThemeState()) }
             BaseView(appTheme.value, systemUiController) {
-                Column(Modifier.background(MaterialTheme.colors.background)) {
+                Column(
+                    modifier = Modifier
+                        .background(MaterialTheme.colors.background)
+                        .verticalScroll(rememberScrollState())
+                ) {
                     TopAppBar(
                         title = { Text("ReadMoreTextView") },
                         navigationIcon = {
@@ -38,11 +56,88 @@ class ReadMoreTextViewActivity : ComponentActivity() {
                             }
                         }
                     )
-                    ExpandableTextView(
-                        text = getString(R.string.expandable_text),
-                        enableActionIcons = true,
-                        enableActionText = true
-                    )
+
+                    Column {
+                        Text(text = "Only Action Text", Modifier.padding(start = 10.dp, top = 10.dp), fontSize = 18.sp)
+                        ExpandableTextView(
+                            text = getString(R.string.expandable_text),
+                            enableActionIcons = false,
+                            enableActionText = true
+                        )
+
+                        Spacer(modifier = Modifier.height(20.dp))
+                        Text(text = "Only Action Icon", Modifier.padding(horizontal = 10.dp), fontSize = 18.sp)
+                        ExpandableTextView(
+                            text = getString(R.string.expandable_text),
+                            enableActionIcons = true,
+                            enableActionText = false
+                        )
+
+                        Spacer(modifier = Modifier.height(20.dp))
+                        Text(text = "Action Text+Icon", Modifier.padding(horizontal = 10.dp), fontSize = 18.sp)
+                        ExpandableTextView(
+                            text = getString(R.string.expandable_text),
+                            enableActionIcons = true,
+                            enableActionText = true
+                        )
+
+                        Spacer(modifier = Modifier.height(20.dp))
+                        Text(text = "Custom Action Icon", Modifier.padding(horizontal = 10.dp), fontSize = 18.sp)
+                        ExpandableTextView(
+                            text = getString(R.string.expandable_text),
+                            enableActionIcons = true,
+                            enableActionText = true,
+                            expandConfig = ExpandConfig(
+                                expandText = "Read More",
+                                expandTextSize = 16.sp,
+                                expandTextColor = Color.Blue,
+                                showUnderline = true,
+                                expandIcon = Icons.Outlined.AccessAlarm
+                            ),
+                            collapseConfig = CollapseConfig(
+                                collapseText = "Read Less", collapseTextSize = 16.sp, collapseTextColor = Color.Blue, showUnderline = true, collapseIcon = Icons.Outlined.Scanner
+                            )
+                        )
+
+                        Spacer(modifier = Modifier.height(20.dp))
+                        Text(text = "Custom Action Text", Modifier.padding(horizontal = 10.dp), fontSize = 18.sp)
+                        ExpandableTextView(
+                            text = getString(R.string.expandable_text),
+                            enableActionIcons = true,
+                            enableActionText = true,
+                            expandConfig = ExpandConfig(
+                                expandText = "Show More",
+                                expandTextSize = 16.sp,
+                                expandTextColor = Color.Blue,
+                                showUnderline = true,
+                                expandIcon = Icons.Outlined.KeyboardArrowDown
+                            ),
+                            collapseConfig = CollapseConfig(
+                                collapseText = "Show Less", collapseTextSize = 16.sp, collapseTextColor = Color.Blue, showUnderline = true, collapseIcon = Icons.Outlined.KeyboardArrowUp
+                            )
+                        )
+
+
+
+
+                        Spacer(modifier = Modifier.height(20.dp))
+                        Text(text = "Custom Action Text Color", Modifier.padding(horizontal = 10.dp), fontSize = 18.sp)
+                        ExpandableTextView(
+                            text = getString(R.string.expandable_text),
+                            enableActionIcons = true,
+                            enableActionText = true,
+                            expandConfig = ExpandConfig(
+                                expandText = "Show More",
+                                expandTextSize = 16.sp,
+                                expandTextColor = Color.Red,
+                                showUnderline = true,
+                                expandIcon = Icons.Outlined.KeyboardArrowDown
+                            ),
+                            collapseConfig = CollapseConfig(
+                                collapseText = "Show Less", collapseTextSize = 16.sp, collapseTextColor = Color.Green, showUnderline = true, collapseIcon = Icons.Outlined.KeyboardArrowUp
+                            )
+                        )
+                    }
                 }
             }
         }
