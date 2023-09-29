12e11408bf774cce9fb56d2312591993ff13aca5
Parashar Dhamecha <parashar@simformsolutions.com>
2023-09-29 14:10:50 +0530

    Added topBar with project theme

--- app/src/main/java/com/jetpack/compose/learning/readmoretextview/ReadMoreTextViewActivity.kt
+++ app/src/main/java/com/jetpack/compose/learning/readmoretextview/ReadMoreTextViewActivity.kt
@@ -3,44 +3,50 @@ package com.jetpack.compose.learning.readmoretextview
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import com.jetpack.compose.learning.R
 import com.jetpack.compose.learning.readmoretextview.components.ExpandableTextView
+import com.jetpack.compose.learning.theme.AppThemeState
+import com.jetpack.compose.learning.theme.BaseView
+import com.jetpack.compose.learning.theme.SystemUiController
 
 
 class ReadMoreTextViewActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ExpandableTextView(
-                text = getString(R.string.expandable_text),
-                enableActionIcons = true,
-                enableActionText = true
-            )
-
-//                RecyclerView()
-//                CardView()
+            val systemUiController = remember { SystemUiController(window) }
+            val appTheme = remember { mutableStateOf(AppThemeState()) }
+            BaseView(appTheme.value, systemUiController) {
+                Column(Modifier.background(MaterialTheme.colors.background)) {
+                    TopAppBar(
+                        title = { Text("ReadMoreTextView") },
+                        navigationIcon = {
+                            IconButton(onClick = { onBackPressed() }) {
+                                Icon(Icons.Filled.ArrowBack, contentDescription = null)
+                            }
+                        }
+                    )
+                    ExpandableTextView(
+                        text = getString(R.string.expandable_text),
+                        enableActionIcons = true,
+                        enableActionText = true
+                    )
+                }
+            }
         }
     }
 }
-/*
-@Composable
-fun CardView() {
-    Card(modifier = Modifier.padding(start = 12.dp, top = 12.dp, end = 12.dp), elevation = CardDefaults.cardElevation()) {
-        ExpandableTextView(
-            text = getString(LocalContext.current, R.string.expandable_text)
-        )
-    }
-}
-
-@Composable
-fun RecyclerView() {
-    LazyColumn {
-        items(20) {
-            CardView()
-        }
-    }
-}
-
- */
 
 
